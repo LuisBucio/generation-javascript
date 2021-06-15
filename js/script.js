d002fc159592f4a51b1c82157ddffdd281950323
Luis Bucio <luis.mtz.bucio@gmail.com>
2021-06-15 17:51:09 -0500

    Ejercicios de JavaScript

---
+++ js/script.js
@@ -0,0 +1,28 @@
+alert('Hola');
+
+var dato1 = 24;
+var dato2 = "hola";
+var dato3 = "fale";
+var dato4 = "5.37";
+var dato5 = true;
+var dato6 = null;
+var dato7 = "Hoy es un lindo día";
+var dato8 = 829;
+var dato9 = "null";
+var dato10 = false;
+
+var suma = parseInt(dato4) + dato8;
+
+console.log('Suma:', suma);
+
+console.log(typeof(dato1));
+console.log(typeof(dato2));
+console.log(typeof(dato3));
+console.log(typeof(dato4));
+console.log(typeof(dato5));
+console.log(typeof(dato6));
+console.log(typeof(dato7));
+console.log(typeof(dato8));
+console.log(typeof(dato9));
+console.log(typeof(dato10));
+console.log(typeof(dato11));
---
+++ js/variables.js
@@ -0,0 +1,51 @@
+alert('Hola de nuevo');
+/*
+var variable1 = 'Soy la variable 1';
+var variable2 = 'Soy la variable 2';
+
+function varTest(){
+    let variable3 = 'Soy la variable 3';
+    var x = 31;
+    if(ture){
+        var x = 71;
+        
+        console.log(x);
+    }
+    console.log(x);
+}
+
+console.log(variable1);*/
+//Una variable puede cambiar su valor, pero una constante no.
+var variable1 = "soy la variable 1";
+console.log(variable1);
+variable1 = 1212;
+console.log(variable1);
+
+const variable2 = 'Variable 2';
+console.log(variable2);
+// variable2 = 10;
+// console.log(variable2);
+
+
+/*Función de flecha*/
+const nuevaFuncion = () => {
+    let variable3 = 'Soy la variable 3';
+    var x = 31;
+    if(ture){
+        var x = 71;
+        
+        console.log(x);
+    }
+    console.log(x);
+
+}
+
+/*Template String*/
+
+const nombre = 'Luis';
+const apellidos = 'Martínez Bucio';
+
+console.log(`Hola mi nombre es ${nombre} y mis apellidos son ${apellidos}`);
+
+
+
